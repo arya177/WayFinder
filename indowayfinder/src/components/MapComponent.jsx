bc09bb64b3e21eb9f255e2415f256ff0acb658e2
Arya Shahi <aryashahi2002@gmail.com>
2023-12-06 13:55:29 +0530

    map recovered

--- indowayfinder/src/components/MapComponent.jsx
+++ indowayfinder/src/components/MapComponent.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -9,23 +10,14 @@ const MapComponent = ({ center, groupMembers }) => {
     const map = L.map('map').setView(center, 22);
 
     // Add a tile layer (you can use any tile provider)
-    // L.tileLayer('/home/arya/Documents/projects/fyp/WayFinder/indowayfinder/src/images/marker.jpg').addTo(map);
-    const imageUrl = '/home/arya/Documents/projects/fyp/WayFinder/indowayfinder/src/images/marker.jpg';
-    // const imageBounds = [[0, 0], [10, 10]]; // Adjust the bounds as needed
-    // L.imageOverlay(imageUrl, imageBounds).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
     // Add markers for each group member
-    // groupMembers.forEach((member) => {
-    //   console.log(member)
-    //   L.marker(member.location).addTo(map).bindPopup(member.name);
-    // });
-    const customMarkerIcon = L.icon({
-      iconUrl: imageUrl,
-      iconSize: [32, 32], // Adjust the size as needed
-      iconAnchor: [16, 32], // Adjust the anchor point as needed
-    });
     groupMembers.forEach((member) => {
-      L.marker(member.location, { icon: customMarkerIcon }).addTo(map).bindPopup(member.name);
+      console.log(member)
+      L.marker(member.location).addTo(map).bindPopup(member.name);
     });
+
     return () => {
       // Clean up when the component unmounts
       map.remove();
